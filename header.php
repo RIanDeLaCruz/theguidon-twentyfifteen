3c1646f37273f2280f0f0b93391a1d8ca0d34b25
Jan Belmonte <jandanbel@gmail.com>
2015-04-11 17:33:52 +0800

    building initial files

---
+++ header.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<!--[if lt IE 9]>
+	<script src="<?php echo esc_url( get_template_directory_uri() ); ?>/js/html5.js"></script>
+	<![endif]-->
+	<script>(function(){document.documentElement.className='js'})();</script>
+	<?php wp_head(); ?>
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+</head>
+<body>
+    <div id="wrap">
+		<header class="header">
+            <p>This is header section. Put your logo and other details here.</p>
+        </header>
